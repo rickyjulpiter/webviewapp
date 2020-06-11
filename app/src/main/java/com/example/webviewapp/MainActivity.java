2ac5435100e5064163fb784f7e069ab35412ae2e
rickyjulpiter <rickyjulpiter14@gmail.com>
2020-06-11 16:04:24 +0700

    init

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.webviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://mymillionfx.com/demo");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
